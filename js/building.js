ccfc77be360ef07accc0213a6fe1a565fa88f132
Michael de Lang <kingoipo@gmail.com>
2014-08-06 19:19:42 +0200

    Start TypeScript-ifying

--- js/building.js
+++ js/building.js
@@ -1,27 +1,35 @@
-function Building(factor, moneyCost, proofCost){
-	this.owned = 0;
-	this.manual = 0;
-	this.factor = factor;
-	this.moneyCost = moneyCost;
-	this.proofCost = proofCost;
-}
+var Building = (function () {
+    function Building(factor, moneyCost, proofCost) {
+        this.owned = 0;
+        this.manual = 0;
+        this.factor = 0;
+        this.factor = factor;
+        this.moneyCost = moneyCost;
+        this.proofCost = proofCost;
+    }
+    Building.prototype.add = function (num) {
+        this.owned += num;
+    };
+    return Building;
+})();
 
-Building.prototype.add = function(num){
-	this.owned += num;
-}
+var CurrBuyable = (function () {
+    function CurrBuyable(cost, text) {
+        this.owned = false;
+        this.cost = cost; //cost is an array of length 6
+    }
+    CurrBuyable.prototype.buy = function () {
+        this.owned = true;
+    };
+    return CurrBuyable;
+})();
 
-function CurrBuyable(cost, text){
-	this.owned = false;
-	this.cost = cost; //cost is an array of length 6
-}
-
-CurrBuyable.prototype.buy = function(){
-	this.owned = true;
-}
-
-function Achievement(name, text, condition){
-	this.achieved = false;
-	this.name = name;
-	this.text = text;
-	this.condition = new Function("return " + condition + ";");
-}
+var Achievement = (function () {
+    function Achievement(name, text, condition) {
+        this.achieved = false;
+        this.name = name;
+        this.text = text;
+        this.condition = new Function("return " + condition + ";");
+    }
+    return Achievement;
+})();
---
+++ js/building.ts
@@ -0,0 +1,43 @@
+class Building {
+	owned: number = 0;
+	manual: number = 0;
+	factor: number = 0;
+	moneyCost: number;
+	proofCost: number;
+	
+	constructor(factor: number, moneyCost: number, proofCost: number) {
+		this.factor = factor;
+		this.moneyCost = moneyCost;
+		this.proofCost = proofCost;
+	}
+	
+	add(num) {
+		this.owned += num;
+	}
+}
+
+class CurrBuyable {
+	owned: boolean = false;
+	cost: Array<number>;
+	
+	constructor(cost: Array<number>, text: string) {
+		this.cost = cost; //cost is an array of length 6
+	}
+	
+	buy() {
+		this.owned = true;
+	}
+}
+
+class Achievement {
+	achieved: boolean = false;
+	name: string;
+	text: string;
+	condition: any;
+
+	constructor(name, text, condition) {
+		this.name = name;
+		this.text = text;
+		this.condition = new Function("return " + condition + ";");
+	}
+}
